68d8fd6043eb562d66826bbb74a477fc36dbf3b2
pockx96 <pock.kriz@gmail.com>
2025-03-13 15:59:48 -0700

    try fix compras

--- Controllers/compras.controller.php
+++ Controllers/compras.controller.php
@@ -1,25 +1,29 @@
 <?php
-class ComprasController {
-    
+class ComprasController
+{
+
     // Obtener todas las compras
-    public static function getAll() {
+    public static function getAll()
+    {
         $sqlString = "SELECT * FROM compras ";
         $query = flight::db()->prepare($sqlString);
         $query->execute();
         $compras = $query->fetchAll();
         Flight::json($compras);
     }
-    
+
     // Obtener compras por cliente
-    public static function getByCliente($cliente) {
-        $sqlString = "SELECT * FROM `compras` WHERE cliente = ? AND deuda>0;";
+    public static function getByCliente($cliente)
+    {
+        $sqlString = "SELECT * FROM `compras` WHERE cliente = ?;";
         $query = flight::db()->prepare($sqlString);
         $query->execute([$cliente]);
         $compras = $query->fetchAll();
         Flight::json($compras);
     }
 
-    public static function getDeuda($cliente){
+    public static function getDeuda($cliente)
+    {
         $sqlString = "SELECT SUM(deuda) AS suma_deuda
         FROM compras
         WHERE cliente = ?;";
@@ -28,41 +32,44 @@ class ComprasController {
         $compras = $query->fetch();
         Flight::json($compras);
     }
-    
+
     // Obtener compras por fecha
-    public static function getByFecha($fecha) {
+    public static function getByFecha($fecha)
+    {
         $sqlString = "SELECT * FROM compras WHERE fecha = ?";
         $query = flight::db()->prepare($sqlString);
         $query->execute([$fecha]);
         $compras = $query->fetchAll();
         Flight::json($compras);
     }
-    
+
     // Agregar una compra
-    public static function post() {
+    public static function post()
+    {
         $request = Flight::request();
         $cliente = $request->data->cliente;
         $tipo_nota = $request->data->tipo_nota;
         $total = $request->data->total;
         $deuda = $request->data->deuda;
-    
+
         $sql = "INSERT INTO compras (folio, fecha, cliente, tipo_nota, total, deuda) 
                 SELECT CONCAT('Fol.', LPAD(COALESCE(MAX(SUBSTR(folio, 5)), 0) + 1, 3, '0')), NOW(), ?, ?, ?, ?
                 FROM compras";
         $query = Flight::db()->prepare($sql);
-    
+
         $query->bindValue(1, $cliente);
         $query->bindValue(2, $tipo_nota);
         $query->bindValue(3, $total);
         $query->bindValue(4, $deuda);
         $query->execute();
-    
+
         Flight::json(["compra creada exitosamente"]);
     }
-    
+
 
     // Eliminar una compra por folio
-    public static function deleteCompra($folio) {
+    public static function deleteCompra($folio)
+    {
         $sqlString = "DELETE FROM compras WHERE folio = ?";
         $query = flight::db()->prepare($sqlString);
         $query->execute([$folio]);
@@ -70,10 +77,11 @@ class ComprasController {
     }
 
     // Actualizar la deuda
-    public static function DeudaUpdate() {
+    public static function DeudaUpdate()
+    {
         $request = Flight::request();
-        $folio = $request->data->folio;    
-        $deuda = $request->data->deuda;    
+        $folio = $request->data->folio;
+        $deuda = $request->data->deuda;
         $sql = "UPDATE compras
         SET deuda = ?
         WHERE folio = ?;";
@@ -81,8 +89,8 @@ class ComprasController {
         $query->bindValue(1, $deuda);
         $query->bindValue(2, $folio);
         $query->execute();
-    
+
         Flight::json(["Deuda actualizada"]);
     }
-    
+
 }
