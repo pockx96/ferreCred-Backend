f8dd82a4760676a745e8fb58f7db95f3283803fc
pockx96 <pock.kriz@gmail.com>
2025-03-19 15:36:13 -0700

    fix GetAll in inventario.controller

--- Controllers/inventario.controller.php
+++ Controllers/inventario.controller.php
@@ -5,25 +5,26 @@ class InventarioController
 
     public static function getAll()
     {
-        $sqlString = "SELECT p.codigo, p.descripcion, p.precio_compra, p.precio_venta, i.cantidad FROM catalogo_producto p JOIN inventario i ON p.codigo = i.codigo_producto;";
+        $sqlString = "SELECT * FROM catalogo_producto";
         $query = Flight::db()->prepare($sqlString);
         $query->execute();
         $entradas = $query->fetchAll();
         Flight::json($entradas);
     }
-    
-    public static function sumarCantidad() {
+
+    public static function sumarCantidad()
+    {
         $request = Flight::request();
         $codigo = $request->data->codigo;
         $cantidad = $request->data->cantidad;
-    
+
         // Buscar si el producto ya existe en la tabla
         $sql_select = "SELECT codigo_producto, cantidad FROM inventario WHERE codigo_producto = ?";
         $query_select = Flight::db()->prepare($sql_select);
         $query_select->bindParam(1, $codigo);
         $query_select->execute();
         $row = $query_select->fetch(PDO::FETCH_ASSOC);
-    
+
         if ($row) {
             // El producto ya existe, actualizar su cantidad
             $sql_update = "UPDATE inventario SET cantidad = cantidad + ? WHERE codigo_producto = ?";
@@ -39,23 +40,24 @@ class InventarioController
             $query_insert->bindParam(2, $cantidad);
             $query_insert->execute();
         }
-    
+
         Flight::json(["mensaje" => "Cantidad actualizada exitosamente"]);
     }
-    
 
-    public static function restarCantidad() {
+
+    public static function restarCantidad()
+    {
         $request = Flight::request();
         $codigo = $request->data->codigo;
         $cantidad_solicitada = $request->data->cantidad;
-    
+
         // Buscar la cantidad actual del producto
         $sql_select = "SELECT cantidad FROM inventario WHERE codigo_producto = ?";
         $query_select = Flight::db()->prepare($sql_select);
         $query_select->bindParam(1, $codigo);
         $query_select->execute();
         $cantidad_actual = $query_select->fetchColumn();
-    
+
         // Verificar si hay suficientes existencias del producto
         if ($cantidad_actual >= $cantidad_solicitada) {
             // Actualizar la cantidad en inventario
@@ -64,14 +66,15 @@ class InventarioController
             $query_update->bindParam(1, $cantidad_solicitada);
             $query_update->bindParam(2, $codigo);
             $query_update->execute();
-    
+
             Flight::json(["mensaje" => "Cantidad actualizada exitosamente"]);
         } else {
             Flight::json(["mensaje" => "No hay suficientes existencias del producto"]);
         }
     }
 
-    public static function editarCantidad() {
+    public static function editarCantidad()
+    {
         $request = Flight::request();
         $codigo = $request->data->codigo;
         $cantidad = $request->data->cantidad;
@@ -96,7 +99,8 @@ class InventarioController
         }
     }
 
-    public static function getByCode($codigo){
+    public static function getByCode($codigo)
+    {
         $sqlString = "SELECT cantidad FROM inventario WHERE codigo_producto = ?";
         $query = Flight::db()->prepare($sqlString);
         $query->execute([$codigo]);
